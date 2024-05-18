57a3cbb2618aaa3494c8a61593c1b95d22908a8b
SuichuKodama <kodama@suichu.net>
2024-05-19 03:21:57 +0900

    アニメーション追加

--- docs/script.js
+++ docs/script.js
@@ -186,11 +186,6 @@ class ThreeApp {
     plane.rotation.x = -Math.PI / 2;
     this.scene.add(plane);
 
-    // 軸ヘルパー
-    const axesBarLength = 5.0;
-    this.axesHelper = new THREE.AxesHelper(axesBarLength);
-    this.scene.add(this.axesHelper);
-
     // コントロール
     // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
